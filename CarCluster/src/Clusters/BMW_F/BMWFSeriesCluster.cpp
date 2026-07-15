7378a898ba74e4b2737f1b2fe8320f286dc4747d
Xiaobai151 <zhangyaodong523@163.com>
2026-07-15 21:24:40 +1000

    Fix Arduino include fragment filenames

--- CarCluster/src/Clusters/BMW_F/BMWFSeriesCluster.cpp
+++ CarCluster/src/Clusters/BMW_F/BMWFSeriesCluster.cpp
@@ -1,9 +1,9 @@
 // BMW F10 implementation split into include fragments to keep repository updates manageable.
 // Project: https://github.com/JackieZ123430/CarCluster-F10-Enhanced
-#include "BMWFSeriesCluster_part00.inc"
-#include "BMWFSeriesCluster_part01.inc"
-#include "BMWFSeriesCluster_part02.inc"
-#include "BMWFSeriesCluster_part03a.inc"
-#include "BMWFSeriesCluster_part03b.inc"
-#include "BMWFSeriesCluster_part04.inc"
-#include "BMWFSeriesCluster_part05.inc"
+#include "BMWFSeriesCluster_part00.h"
+#include "BMWFSeriesCluster_part01.h"
+#include "BMWFSeriesCluster_part02.h"
+#include "BMWFSeriesCluster_part03a.h"
+#include "BMWFSeriesCluster_part03b.h"
+#include "BMWFSeriesCluster_part04.h"
+#include "BMWFSeriesCluster_part05.h"
